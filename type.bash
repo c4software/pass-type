08ee30c36ecf848e92d3c3c12e6cd05869b6f067
Valentin Brosseau <c4software@gmail.com>
2018-03-19 13:12:13 +0100

    Add countdown instead of static number

--- type.bash
+++ type.bash
@@ -7,8 +7,13 @@ check_sneaky_paths "$path"
 
 if [[ -f $passfile ]]; then
     local temporary=$($GPG -d "${GPG_OPTS[@]}" "$passfile" | tail -n +1 | head -n 1 | sed -e 's/\\/\\\\/g' | sed -e 's/"/\\"/g' )
-    echo "The password will be type in $delay seconds"
-    sleep $delay
+
+    while [ $delay -gt 0 ]; do
+        echo -ne "The password will be type in $delay seconds.\033[0K\r"
+        sleep 1
+        delay=$((delay-1))
+    done
+
     if [[ `uname` == 'Darwin' ]]
     then
         osascript -e "tell application \"System Events\" to keystroke \"$temporary\""
